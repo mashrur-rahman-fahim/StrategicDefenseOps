f17602d9e030be3e1fa65f3ee1f892bee78f8b37
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-02-22 03:07:03 +0600

    Audit Log : NewPasswordController : Password Reset!

--- backend/app/Http/Controllers/Auth/NewPasswordController.php
+++ backend/app/Http/Controllers/Auth/NewPasswordController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
 use Illuminate\Validation\ValidationException;
+use Spatie\Activitylog\Facades\Activity;
 
 class NewPasswordController extends Controller
 {
@@ -39,10 +40,25 @@ class NewPasswordController extends Controller
                 ])->save();
 
                 event(new PasswordReset($user));
+                // Audit Log : successful password reset
+                Activity::create([
+                    'log_name' => 'password_reset',
+                    'user_name' => $user->name,
+                    'user_email' => $user->email,
+                    'description' => 'User successfully reset their password.',
+                    'subject_type' => 'App\Models\User',
+                    'subject_id' => $user->id,
+                    'causer_type' => 'App\Models\User',
+                    'causer_id' => $user->id,
+                    'properties' => json_encode([
+                        'method' => 'email reset',
+                    ])
+                ]);
             }
         );
 
         if ($status != Password::PASSWORD_RESET) {
+             
             throw ValidationException::withMessages([
                 'email' => [__($status)],
             ]);
