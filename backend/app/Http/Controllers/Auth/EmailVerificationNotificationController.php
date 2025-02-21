1e8724cfb1ba02d89fab22c58e71367a32446726
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-02-22 03:34:23 +0600

    Audit Log : EmailVerificationNotificationController : Email Verified!

--- backend/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ backend/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Spatie\Activitylog\Facades\Activity;
 
 class EmailVerificationNotificationController extends Controller
 {
@@ -16,6 +17,21 @@ class EmailVerificationNotificationController extends Controller
     public function store(Request $request): JsonResponse|RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
+            // Audit Log : when the user already has verified email
+            Activity::create([
+                'log_name' => 'email_verification_notificaiton',
+                'user_name' => $request->user()->name,
+                'user_email' => $request->user()->email,
+                'description' => 'Tried to send verification email, but the email is already verified.',
+                'subject_type' => 'App\Models\User',
+                'subject_id' => $request->user()->id,
+                'causer_type' => 'App\Models\User',
+                'causer_id' => $request->user()->id,
+                'properties' => json_encode([
+                    'status' => 'already_verified',
+                    'timestamp' => now()->toDateTimeString(),
+                ])
+            ]);
             return redirect()->intended(RouteServiceProvider::HOME);
         }
 
