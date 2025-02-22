ef6148a14747cfb22f5524fae570f66ee38b5cf5
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-02-22 16:21:13 +0600

    Audit Log : EmailVerificationNotificationController fixed!

--- backend/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ backend/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Spatie\Activitylog\Facades\Activity;
+use Illuminate\Support\Str;
+
 
 class EmailVerificationNotificationController extends Controller
 {
@@ -20,8 +22,10 @@ class EmailVerificationNotificationController extends Controller
             // Audit Log : when the user already has verified email
             Activity::create([
                 'log_name' => 'email_verification_notificaiton',
+                'user_id' => $request->user()->id,
                 'user_name' => $request->user()->name,
                 'user_email' => $request->user()->email,
+                'role_id' => $request->user()->role_id,
                 'description' => 'Tried to send verification email, but the email is already verified.',
                 'subject_type' => 'App\Models\User',
                 'subject_id' => $request->user()->id,
@@ -30,7 +34,11 @@ class EmailVerificationNotificationController extends Controller
                 'properties' => json_encode([
                     'status' => 'already_verified',
                     'timestamp' => now()->toDateTimeString(),
-                ])
+                ]),
+                'event' => 'email_verification_notified',
+                'batch_uuid' => Str::uuid()->toString(),
+                'created_at' => now(),
+                'updated_at' => now(),
             ]);
             return redirect()->intended(RouteServiceProvider::HOME);
         }
@@ -40,8 +48,10 @@ class EmailVerificationNotificationController extends Controller
         // Audit Log : when the verification link is sent
         Activity::create([
             'log_name' => 'email_verification',
+            'user_id' => $request->user()->id,
             'user_name' => $request->user()->name,
             'user_email' => $request->user()->email,
+            'role_id' => $request->user()->role_id,
             'description' => 'Verification email sent.',
             'subject_type' => 'App\Models\User',
             'subject_id' => $request->user()->id,
@@ -50,7 +60,11 @@ class EmailVerificationNotificationController extends Controller
             'properties' => json_encode([
                 'status' => 'verification_sent',
                 'timestamp' => now()->toDateTimeString(),
-            ])
+            ]),
+            'event' => 'email_verification_sent',
+            'batch_uuid' => Str::uuid()->toString(),
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         return response()->json(['status' => 'verification-link-sent']);
