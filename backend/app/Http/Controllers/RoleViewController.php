44b42454524f04fbe540cdbdde4e8a8e5e1a2429
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-03-03 07:38:24 +0600

    up to date

--- backend/app/Http/Controllers/RoleViewController.php
+++ backend/app/Http/Controllers/RoleViewController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class RoleViewController extends Controller
 {
     protected RoleViewService $roleViewService;
-    public function __construct(RoleViewService $roleViewService){
+     public function __construct(RoleViewService $roleViewService){
         $this->roleViewService = $roleViewService;
     }
     public function roleView(){
