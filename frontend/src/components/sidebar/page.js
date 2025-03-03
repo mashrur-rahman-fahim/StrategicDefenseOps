12dac1321d0a70b878dc4ab07b536d032bdca2c7
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-03-03 18:46:47 +0600

    merge conflict resolved

--- frontend/src/components/sidebar/page.js
+++ frontend/src/components/sidebar/page.js
@@ -1,19 +1,5 @@
 'use client'
 import React, { useEffect, useRef, useState } from 'react'
-<<<<<<< HEAD:frontend/src/app/(app)/components/sidebar/page.js
-import { useRouter } from 'next/navigation' // ✅ Import Next.js Router
-import axios from '../../../../lib/axios'
-import './sidebar.css'
-
-const Sidebar = ({ isOpen, toggleSidebar, selectedItem, setSelectedItem }) => {
-    const sidebarRef = useRef();
-    const router = useRouter(); // ✅ Initialize Router
-
-    const [userName, setUserName] = useState('Error');
-    const [roleId, setRoleId] = useState(null);
-    const [roleName, setRoleName] = useState('');
-
-=======
 import axios from '@/lib/axios'
 import './sidebar.css'
 
@@ -21,13 +7,12 @@ const Sidebar = ({ isOpen, toggleSidebar, selectedItem, handleNavigation }) => {
     const sidebarRef = useRef()
     const [userName, setUserName] = useState('Error')
     const [roleName, setRoleName] = useState("")
->>>>>>> c343e0f389afd120f8d02f0e36d297f2e7a5cdfc:frontend/src/components/sidebar/page.js
     const roleMapping = {
         1: 'Admin',
         2: 'Manager',
         3: 'Operator',
         4: 'Viewer',
-    };
+    }
 
     useEffect(() => {
         const handleClickOutside = event => {
@@ -36,39 +21,31 @@ const Sidebar = ({ isOpen, toggleSidebar, selectedItem, handleNavigation }) => {
                 sidebarRef.current &&
                 !sidebarRef.current.contains(event.target)
             ) {
-                toggleSidebar();
+                toggleSidebar()
             }
-        };
+        }
 
-        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('mousedown', handleClickOutside)
         return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [isOpen, toggleSidebar]);
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [isOpen, toggleSidebar])
 
     useEffect(() => {
         const fetchUserDetails = async () => {
             try {
                 const response = await axios.get(
-<<<<<<< HEAD:frontend/src/app/(app)/components/sidebar/page.js
-                    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user`,
-                );
-                setUserName(response.data.name || 'Unknown');
-                setRoleId(response.data.role_id || 'Unknown');
-                setRoleName(roleMapping[response.data.role_id] || 'Unknown');
-=======
                     `/api/user`,
                 )
                 setUserName(response?.data.name || 'Unknown')
                 setRoleName(roleMapping[response.data.role_id] || 'Unknown')
->>>>>>> c343e0f389afd120f8d02f0e36d297f2e7a5cdfc:frontend/src/components/sidebar/page.js
             } catch (error) {
-                console.error('Error fetching user details:', error);
+                console.error('Error fetching user details:', error)
             }
-        };
+        }
 
-        fetchUserDetails();
-    }, []);
+        fetchUserDetails()
+    }, [])
 
     return (
         <div ref={sidebarRef} className={`sidebar ${isOpen ? 'open' : ''}`}>
@@ -78,6 +55,14 @@ const Sidebar = ({ isOpen, toggleSidebar, selectedItem, handleNavigation }) => {
 
             {/* Profile  */}
             <div className="profile">
+                {/* Profile Picture */}
+                {/* <img
+          src="https://www.lexpress.fr/resizer/BtP_kaudrTSss-e5I9p9gCwh0gk=/arc-photo-lexpress/eu-central-1-prod/public/ENBOXHVYU5EOVLIYIJ6NNM5ET4.jpg"
+          alt="Profile"
+          className="profile-pic"
+        /> */}
+
+                {/* Profile Name */}
                 <h2>{userName}</h2>
                 <p className="rank">Role : {roleName}</p>
             </div>
@@ -89,14 +74,8 @@ const Sidebar = ({ isOpen, toggleSidebar, selectedItem, handleNavigation }) => {
                             key={item}
                             className={`menu-item ${selectedItem === item.toLowerCase() ? 'active' : ''}`}
                             onClick={() => {
-<<<<<<< HEAD:frontend/src/app/(app)/components/sidebar/page.js
-                                setSelectedItem(item.toLowerCase());
-                                toggleSidebar();
-                                router.push(`/${item.toLowerCase()}`); // ✅ Navigate to page
-=======
                                 handleNavigation(item.toLowerCase())
                                 toggleSidebar()
->>>>>>> c343e0f389afd120f8d02f0e36d297f2e7a5cdfc:frontend/src/components/sidebar/page.js
                             }}>
                             {item}
                         </div>
@@ -104,7 +83,7 @@ const Sidebar = ({ isOpen, toggleSidebar, selectedItem, handleNavigation }) => {
                 )}
             </nav>
         </div>
-    );
-};
+    )
+}
 
-export default Sidebar;
+export default Sidebar
