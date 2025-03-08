2badf1fce7b114f96675789f84d5cfb88bf2b39e
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-03-08 20:27:59 +0600

    this branch purpose ccompleted

--- frontend/src/components/sidebar/page.js
+++ frontend/src/components/sidebar/page.js
@@ -103,7 +103,7 @@ const Sidebar = ({ isOpen, toggleSidebar, selectedItem, handleNavigation }) => {
 
             {/* Menu items */}
             <nav className="menu">
-                {["Dashboard", "Resources", "Operation", "Reports", "AuditLog", "Notifications"].map(
+                {["Dashboard", "Resources", "Operation", "Reports", "AuditLog", "Notifications", "Chatbot"].map(
                     (item) => (
                         <div
                             key={item}
